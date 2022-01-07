e8b46e2b958dde7ff2ce390d7ab7d53aaddf326c
sezer bingöl <sezerbingol@arentech.com.tr>
2022-01-07 14:46:39 +0300

    Add project files.

---
+++ TelefonRehberi/Content/JS/getlistangular.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('myApp', []);
+
+app.controller('myControllerPersonel', function ($scope, $http) {
+    $http.get("/Admin/GetPersonelList/").then(
+       function (response) {
+           $scope.personelList = response.data;
+       });
+    $http.get("/Admin/GetDepartmanList/").then(
+       function (response) {
+           $scope.departmanList = response.data;
+       });
+});
